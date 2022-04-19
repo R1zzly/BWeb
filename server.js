3b64ae8a23b7d7ed26369ef536f5c3f9d6ff60d8
R1zzly <rizzlychannel@gmail.com>
2022-04-19 22:27:36 +0600

    routers for other html

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use('/', express.static('public'));
 
 app.use("/", require("./public/main.js"));
 app.use("/shop", require("./public/shop.js"));
+app.use("/about", require("./public/about.js"));
+app.use("/blog", require("./public/blog.js"));
 app.use("/sproduct", require("./public/sproduct.js"));
 app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`)
