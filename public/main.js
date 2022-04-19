04848aa13771ae1dc7db9bde8e583d09524f9673
R1zzly <rizzlychannel@gmail.com>
2022-04-19 23:19:01 +0600

    р

--- public/main.js
+++ public/main.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const bodyParser = require('body-parser');
+const https = require('https');
 router
     .route("/")
     .get((req, res) => res.sendfile("public/main.html"))
-    .post((req, res) => res.send("POST"));
+    .post((req, res) =>  res.send("POST"));
 module.exports = router;
