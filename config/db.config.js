35e930eadfd8ac99a4fa7d255dfa84ddc42375de
R1zzly <93976440+R1zzly@users.noreply.github.com>
2022-05-12 23:51:03 +0600

    z

---
+++ config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://admin-Zhandos:Qwerty12345@cluster0.6mwif.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const https = require('https')
+const express = require("express");
+const bodyparser = require("body-parser");
+const mongoose = require('mongoose');
+const path = require('path');
+const ejs = require('ejs');
+const config = require('./config/db.config');
 
-const app = express()
-app.set("view engine",'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
-const port = 3000
-const regRoute = require('./routes/auth');
-app.use('/', regRoute);
-const logRoute = require('./routes/login');
-app.use('/', logRoute);
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static('public'));
-app.use('/img', express.static('public'));
-app.use('/', express.static('public'));
+mongoose.connect(config.mongoURI).then(() => console.log("Database connected successfully"))
+    .catch((err) => console.log(err));
 
-app.use("/", require("./public/main.js"));
-app.use("/shop", require("./public/shop.js"));
-app.use("/about", require("./public/about.js"));
-app.use("/blog", require("./public/blog.js"));
-app.use("/sproduct", require("./public/sproduct.js"));
-app.use("/contact", require("./public/contact.js"));
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine','ejs');
+
+app.use("/", require("./routes/root.js"));
+app.use("/about", require("./routes/about.js"));
+app.use("/shop", require("./routes/shop.js"));
+app.use("/blog", require("./routes/blog.js"));
+app.use("/product", require("./routes/product.js"));
+
 app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`)
 );
